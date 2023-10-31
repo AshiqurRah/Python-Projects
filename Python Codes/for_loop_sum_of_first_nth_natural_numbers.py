b53a2640fbbc390321de96ec1a3ecb1e4e4db054
Md. Ashiqur Rahman <62054530+ashiqur0202@users.noreply.github.com>
2023-10-31 22:35:26 +0600

    for loop

---
+++ Python Codes/for_loop_sum_of_first_nth_natural_numbers.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a positive integer n: "))
+
+sum_of_natural_numbers = 0
+
+for i in range(1, n+1):
+    sum_of_natural_numbers += i
+
+print(f"The sum of the first {n} natural numbers s {sum_of_natural_numbers}")
